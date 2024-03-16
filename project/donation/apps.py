48f18a1a39588b6b7fe80e5395dc9e42169ea732
1 <margoloko@mail.ru>
2024-03-16 18:21:54 +0700

    chore: Установлен pre-commit, обновлен requirements.txt, обновлены настройки, добавлены приложения.

--- project/donation/apps.py
+++ project/donation/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class DonationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'donation'
+    """Настройка приложения donation."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "donation"
+    verbose_name = "Денежные сборы"
