9d75fc3a4029cea7029eae1460a535f070c26b9a
1 <margoloko@mail.ru>
2024-03-18 18:05:23 +0700

    feature: Добавлена конфигурация админки.

--- project/donation/admin.py
+++ project/donation/admin.py
@@ -7,11 +7,29 @@ from .models import Collect, Payment
 class CollectAdmin(admin.ModelAdmin):
     """Админка для модели Collect."""
 
-    pass
+    list_display = ("title", "author", "occasion", "current_sum", "goal", "deadline")
+    list_filter = ("title", "author", "occasion", "goal", "deadline", "timestamp")
+    search_fields = ("title", "author", "occasion", "goal", "deadline")
+    ordering = (
+        "title",
+        "author",
+        "occasion",
+        "current_sum",
+        "timestamp",
+        "goal",
+        "deadline",
+    )
+    readonly_fields = "timestamp"
+    empty_value_display = "-пусто-"
 
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
     """Админка для модели Payment."""
 
-    pass
+    list_display = ("user", "amount", "collect")
+    list_filter = ("user", "amount", "collect", "timestamp")
+    search_fields = ("user", "amount", "collect", "timestamp")
+    ordering = ("user", "amount", "collect", "timestamp")
+    readonly_fields = "timestamp"
+    empty_value_display = "-пусто-"
